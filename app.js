d9421eb13343afb321283356edb8922f3a158d84
AbdiDR <abdidwiramdani@gmail.com>
2023-06-03 01:21:13 +0700

    ADD CORS

--- app.js
+++ app.js
@@ -2,27 +2,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors'); // Import the cors package
 
-const db = require('./database/models')
+const db = require('./database/models');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/userRoute');
-const todosRouter = require('./routes/todoRoute')
+const todosRouter = require('./routes/todoRoute');
 
 const app = express();
 
 try {
-    db.sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  db.sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Add CORS middleware before your routes
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
